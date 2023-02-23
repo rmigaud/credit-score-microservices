05633a8630f35cfa006f2c2b169e8d54ba45fbd2
ryan <rmigaud@asu.edu>
2023-02-22 20:04:55 -0800

    hides db credentials in secrets.properties so application.properties can be included.

--- src/main/java/rmigaud/creditscore/entities/CreditScore.java
+++ src/main/java/rmigaud/creditscore/entities/CreditScore.java
@@ -23,4 +23,35 @@ public class CreditScore {
     this.score = score;
   }
 
+  public String getSsn() {
+    return ssn;
+  }
+
+  public void setSsn(String ssn) {
+    this.ssn = ssn;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
 }
