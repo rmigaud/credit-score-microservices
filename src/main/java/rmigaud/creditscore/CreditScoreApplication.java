831f1cf8c30d8e248d06d982f1468819c8d48be6
ryan <rmigaud@asu.edu>
2023-02-27 21:41:11 -0800

    hides db credentials in secrets.properties so application.properties can be included.

--- src/main/java/rmigaud/creditscore/CreditScoreApplication.java
+++ src/main/java/rmigaud/creditscore/CreditScoreApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreditscoreApplication {
+public class CreditScoreApplication {
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreditscoreApplication.class, args);
+		SpringApplication.run(CreditScoreApplication.class, args);
 	}
 
 }
